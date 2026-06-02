5037c15bd8296eb0168d8c9eabdcd4b0b12f8156
Jatins01 <jatinsolanki1106@gmail.com>
2026-06-02 21:12:11 +0530

    Testing CI/CD Pipeline

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080 // if PORT given otherwise i use 8080
 
 app.get('/', (req, res) => {         //make root and recieve request and response
-    return res.json({ msg: 'Hello from the server'}) //set response in json that gives message
+    return res.json({ msg: 'Hello from the server - CI/CD Test'}) //set response in json that gives message
 })
 
 app.listen(PORT, () => {
